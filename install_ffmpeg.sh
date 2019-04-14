ac7d4f0b997ffbacf0b6348a410da853009b410f
elerator <43006409+elerator@users.noreply.github.com>
2019-04-14 15:21:49 +0200

    Update install_ffmpeg.sh

--- install_ffmpeg.sh
+++ install_ffmpeg.sh
@@ -16,9 +16,7 @@ cd source
 #install required packages
 sudo apt-get -y install autoconf automake build-essential libass-dev libfreetype6-dev \
 	libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev \
-	libxcb-xfixes0-dev pkg-config texinfo zlib1g-devsudo apt-get -y install autoconf automake build-essential libass-dev libfreetype6-dev \
-	libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev \
-	libxcb-xfixes0-dev pkg-config texinfo zlib1g-dev
+	libxcb-xfixes0-dev pkg-config texinfo zlib1g-dev;
 
 #install yasm compiler
 sudo apt install yasm
